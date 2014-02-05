34b5c0e2aee9e32d8b48ff2d7c4d0004ac91ecd7
Michel Oosterhof <michel@oosterhof.net>
2014-02-05 14:06:36 +0100

    updates

--- tools/xor.c
+++ tools/xor.c
@@ -8,45 +8,97 @@
 
 /*
  * XOR TODO: accept stdin TODO: accept strings on command line TODO: wrap
- * everything but the largest input 
+ * everything but the largest input/stdin
  */
+
+enum type { file, xstdin, byte, string };
+
+struct source {
+    enum type sourcetype;
+    char *data;
+    int fd;
+    off_t size;     
+    int exhausted;
+    struct source *next;
+};
+
+struct source *head = NULL;
 
 void
 usage(char *name)
 {
-    fprintf(stderr, "%s [-o <outputfile>] <file> <file> [file3]\n", name);
+    fprintf(stderr,
+	    "%s [-b <byte>] [-o <outputfile>] <file> <file> [file3]\n",
+	    name);
 }
 
+int
+xmmap(char **map, int *fd, off_t * size, const char *filename)
+{
+    int             fd1;
+    struct stat     st1;
+
+    fd1 = open(filename, O_RDONLY);
+    fprintf(stderr, "inputfile = %s\n", filename);
+    if (fd1 == -1) {
+	perror("Error opening file1 for reading");
+	exit(2);
+    }
+    if (fstat(fd1, &st1) == -1) {
+	perror("Can't determine file1 size");
+	exit(3);
+    }
+    *map = mmap(NULL, st1.st_size, PROT_READ, MAP_SHARED, fd1, 0);
+    if (map == MAP_FAILED) {
+	perror("Can't map file1");
+	exit(4);
+    }
+
+    *fd = fd1;
+    *size = st1.st_size;
+    return 0;
+}
 
 int
 main(int argc, char *argv[])
 {
 
-    ssize_t         i;
-    struct stat     st1,
-                    st2,
-                    st3,
-                    sto;
+    off_t           i,
+                    size1,
+                    size2,
+                    size3;
+
     int             ifiles = 0,
-	sources = 0,
+		    ibytes = 0,
+		    istdin = 0,
+			isources = 0;
+    int
 	fd1,
 	fd2,
 	fd3,
 	fdo;
 
     int             c,
-                    index;
+	byte = 0,
+	index;
     char           *outputfile = NULL;
     char           *file1,
                    *file2,
                    *file3;
 
-    while ((c = getopt(argc, argv, "o:")) != -1)
+    while ((c = getopt(argc, argv, "b:o:")) != -1)
 	switch (c) {
+	case 'b':
+	    byte = atoi(optarg);
+	    ibytes = 1;
+	    break;
 	case 'o':
 	    outputfile = optarg;
 	    break;
 	case '?':
+	    if (optopt == 'b')
+		fprintf(stderr, "Option -%c requires an argument.\n",
+			optopt);
 	    if (optopt == 'o')
 		fprintf(stderr, "Option -%c requires an argument.\n",
 			optopt);
@@ -65,9 +117,9 @@ main(int argc, char *argv[])
 	ifiles++;
     }
 
-    sources = ifiles;
+    isources = ifiles + ibytes + istdin;
 
-    if (sources != 2 && sources != 3) {
+    if (isources < 2) {
 	usage(argv[0]);
 	exit(1);
     }
@@ -86,65 +138,46 @@ main(int argc, char *argv[])
 	}
     }
 
-    fd1 = open(argv[optind], O_RDONLY);
-    fprintf(stderr, "inputfile = %s\n", argv[optind]);
-    if (fd1 == -1) {
-	perror("Error opening file1 for reading");
-	exit(2);
-    }
-    if (fstat(fd1, &st1) == -1) {
-	perror("Can't determine file1 size");
-	exit(3);
-    }
-    file1 = mmap(NULL, st1.st_size, PROT_READ, MAP_SHARED, fd1, 0);
-    if (file1 == MAP_FAILED) {
-	perror("Can't map file1");
-	exit(4);
-    }
+    if (ifiles > 0) xmmap(&file1, &fd1, &size1, argv[optind]);
+    if (ifiles > 1) xmmap(&file2, &fd2, &size2, argv[optind + 1]);
+    if (ifiles > 2) xmmap(&file3, &fd3, &size3, argv[optind + 2]);
+
+    if (ifiles=1) largest = 0;
+    if (ifiles=1)
+
+    moredata = 1;
+    do {
+
+        if (ifiles>0) {
+		 ret1 = read( fd1, buf1, 1 );
+		 if ( ret1 == 0 ) { // wrap
+			lseek( fd1, SEEK_SET, 0  );
+		 }
+        }
+
+        if (ifiles>1) ret2 = read( fd2, buf2, 1 );
+		 ret2 = read( fd2, buf2, 1 );
+		 if ( ret2 == 0 ) { // wrap
+			lseek( fd2, SEEK_SET, 0  );
+		 }
 
 
-    fd2 = open(argv[optind + 1], O_RDONLY);
-    fprintf(stderr, "inputfile = %s\n", argv[optind + 1]);
-    if (fd2 == -1) {
-	perror("Error opening file2 for reading");
-	exit(2);
-    }
-    if (fstat(fd2, &st2) == -1) {
-	perror("Can't determine file2 size");
-	exit(3);
-    }
-    file2 = mmap(NULL, st2.st_size, PROT_READ, MAP_SHARED, fd2, 0);
-    if (file2 == MAP_FAILED) {
-	perror("Can't map file2");
-	exit(4);
-    }
+        if (ifiles>2) ret3 = read( fd3, buf3, 1 );
+		 ret3 = read( fd3, buf3, 1 );
+		 if ( ret3 == 0 ) { // wrap
+			lseek( fd3, SEEK_SET, 0  );
+		 }
+
+        if (istdin>0) ret4 = read( 0, buf4, 1 );
+		 if ( ret4 = 0 ) { // the-end 
+			moredata = 0;
+		 }
 
 
+    } while( moredata ) 
 
-    if (ifiles > 2) {
-	fd3 = open(argv[optind + 2], O_RDONLY);
-	fprintf(stderr, "inputfile = %s\n", argv[optind + 2]);
-	if (fd3 == -1) {
-	    perror("Error opening file3 for reading");
-	    exit(2);
-	}
-	if (fstat(fd3, &st3) == -1) {
-	    perror("Can't determine file3 size");
-	    exit(3);
-	}
-	file3 = mmap(NULL, st3.st_size, PROT_READ, MAP_SHARED, fd3, 0);
-	if (file3 == MAP_FAILED) {
-	    perror("Can't map file3");
-	    exit(4);
-	}
-    }
-    // size1==size2 or exit
-    if (fstat(fdo, &sto) == -1) {
-	perror("Can't determine fileo size");
-	exit(3);
-    }
 
-    for (i = 0; i < st1.st_size; i++) {
+    for (i = 0; i < size1; i++) {
 	char            xor[2];
 	/*
 	 * Binary operation goes here. If speed is an issue, you may want
@@ -169,17 +202,16 @@ main(int argc, char *argv[])
 	}
     }
 
-    munmap(file1, st1.st_size);
+    munmap(file1, size1);
     close(fd1);
 
-    munmap(file2, st2.st_size);
+    munmap(file2, size2);
     close(fd2);
 
     if (ifiles > 2) {
-	munmap(file3, st3.st_size);
+	munmap(file3, size3);
 	close(fd3);
     }
-    // munmap(fileo, sto.st_size);
     close(fdo);
     return 0;
 }
