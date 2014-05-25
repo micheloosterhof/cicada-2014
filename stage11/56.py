5dec851796912180a4ffacef3fdfa554c7a27461
Michel Oosterhof <michel@oosterhof.net>
2014-05-25 20:51:48 +0200

    add 56.py and 56.runes

--- stage11/56.py
+++ stage11/56.py
@@ -4,15 +4,22 @@
 # number to gematria
 n2g = { 0: 'F', 1: 'U', 2: 'TH', 3: 'O', 4: 'R', 5: 'C/K', 6: 'G', 7: 'W', 8: 'H', 9: 'N', 10: 'I', 11: 'J', 12: 'EO', 13: 'P', 14: 'X', 15: 'S/Z', 16: 'T', 17: 'B', 18: 'E', 19: 'M', 20: 'L', 21: 'NG/ING', 22: 'OE', 23: 'D', 24: 'A', 25: 'AE', 26: 'Y', 27: 'IA/IO', 28: 'EA' }
 
+skip = 56
+
 def main():
+    counter = 0
     primes = iter( gen_primes() ) 
     parts = open( '56.runes', 'r' ).read().split()
     for rune in parts:
         if rune.isdigit():
             rune = int(rune)
+            if counter == skip:
+                print 'OUT:', n2g[rune], "  ", counter, "th rune (SKIP)"
+                counter=counter+1
+                continue
             prime = primes.next();
-            # print 'IN:', int(rune), "--", n2g[int(rune)]
-            print 'OUT:', n2g[(((int(rune)-prime)) % 29 )+1]
+            print 'OUT:', n2g[(((int(rune)-prime)) % 29 )+1], "  ", counter, "th rune"
+            counter=counter+1
         else:
             print ":",rune,":"
 
--- stage11/ky2khlqdf7qdznac.onion/README.md
+++
@@ -1,4 +0,0 @@
-
-59.txt contains text from page 59.
-
-    xxd -r -p 59.txt > 59.bin
