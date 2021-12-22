8f22c45ad14c0520cea08479378e8c1476180c88
Michel Oosterhof <michel@MacBook-Pro.local>
2021-12-22 09:22:37 +0800

    convert to python3, clean up and rename

--- stage11/decode56.py
+++ stage11/decode56.py
@@ -1,28 +1,29 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 # input file should contain runes encoded as numbers, separated by whitespace. Encoding starts with 0
 
 # number to alphabet
 n2g = { 0: 'F', 1: 'U', 2: 'TH', 3: 'O', 4: 'R', 5: 'C/K', 6: 'G', 7: 'W', 8: 'H', 9: 'N', 10: 'I', 11: 'J', 12: 'EO', 13: 'P', 14: 'X', 15: 'S/Z', 16: 'T', 17: 'B', 18: 'E', 19: 'M', 20: 'L', 21: 'NG/ING', 22: 'OE', 23: 'D', 24: 'A', 25: 'AE', 26: 'Y', 27: 'IA/IO', 28: 'EA' }
 
-skip = 56
-addition = 1
+SKIP = 56
+ADDITION = 1
 
 def main():
     counter = 0
-    primes = iter( gen_primes() ) 
-    parts = open( '56.runes', 'r' ).read().split()
+    primes = iter( gen_primes() )
+    with open( '56.runes', 'r', encoding="latin-1") as runes:
+        parts = runes.read().split()
     for rune in parts:
         if rune.isdigit():
             rune = int(rune)
-            if counter == skip:
-                print counter, ':', n2g[rune], "  (SKIP DECODE)"
+            if counter == SKIP:
+                print( counter, ':', n2g[rune], "  (SKIP DECODE)")
                 counter=counter+1
                 continue
-            prime = primes.next();
-            print counter, ':', n2g[(rune-prime+addition) % 29]
+            prime = next(primes)
+            print( counter, ':', n2g[(rune-prime+ADDITION) % 29])
             counter=counter+1
         else:
-            print ":",rune,":"
+            print( ":",rune,":")
 
 
 # Sieve of Eratosthenes
@@ -31,11 +32,11 @@ def main():
 def gen_primes():
     """ Generate an infinite sequence of prime numbers.
     """
-    D = {}  
-    q = 2  
+    D = {}
+    q = 2
     while True:
         if q not in D:
-            yield q        
+            yield q
             D[q * q] = [q]
         else:
             for p in D[q]:
@@ -44,6 +45,5 @@ def gen_primes():
 
         q += 1
 
-# main
-main()
 
+main()
